5ffe79f5f118b42e6c66707885b0ed1c3f7f0bee
Brosner522 <rosnerbb@gmail.com>
2021-04-05 21:18:09 -0400

    commit tree 3e4e45fcf18027306d1f9a94a968c929b6aecba3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(source, sought) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === sought.toLowerCase()
+    )
+  }
+
+  function fuzzyMatch(drivers) {
+      drivers.filter(possiblematch + anotherPossibleMatch);
+    possibleMatch = drivers[0] ==='S';
+    anotherPossibleMatch = drivers[1] === 'a';
+  }
+
